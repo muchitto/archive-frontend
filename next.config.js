dfab5268408c99b4ded10de7862f64c77f59a82c
muchitto <>
2022-09-18 15:53:01 +0300

    No need for the other redis package

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'archive.org',
-      'ia600803.us.archive.org',
-      'ia800803.us.archive.org',
-      'ia800204.us.archive.org',
-      'ia600904.us.archive.org',
-    ]
+    domains: [ 'archive.org', 'ia600803.us.archive.org', 'ia800803.us.archive.org', 'ia800204.us.archive.org', 'ia600904.us.archive.org', ]
   },
 };
 
